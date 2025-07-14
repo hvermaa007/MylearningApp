a126503b413357cb26d967b1b0d99dfb9c4c7e5a
hverma007 <harshitvermaa@hotmail.com>
2025-07-14 19:42:51 -0400

    changed code for fail test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('hi, World!');
 });
 
 // New route with a linting issue: unused variable
