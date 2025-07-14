66de1ded706a157bfd58b987f6ddacdda3845ea8
hverma007 <harshitvermaa@hotmail.com>
2025-07-14 19:16:49 -0400

    Node js app with unit test,coverhgae and lint

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// New route with a linting issue: unused variable
+app.get('/ping', (req, res) => {
+  const unused = 42; // ESLint will warn about 'unused' being defined but never used
+  res.send('pong');
+});
+
+// app.listen(3000, () => {
+//   console.log(Server running at http://localhost:3000);
+// });
+module.exports=app;
