7e25376caf290227cd8cefe03dbbb5c826e8aa49
hverma007 <harshitvermaa@hotmail.com>
2025-07-14 19:48:59 -0400

    changed code coverage %

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hi, World!');
+  res.send('Hello, World!');
 });
 
 // New route with a linting issue: unused variable
